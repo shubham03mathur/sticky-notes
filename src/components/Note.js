8405ac09198ae01afdb1045e030264d7b90ece82
Shubham <coder_shubham@outlook.com>
2024-09-29 16:03:16 +0530

    Add smooth trnasition for sticky notes

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,24 +1,35 @@
+/* eslint-disable react/display-name */
 /* eslint-disable react/prop-types */
-const Note = ({ content, initialPosition, ...props}) => {
-  return (
-    <div style={{
-        position: 'absolute',
-        padding: '10px',
-        left: `${initialPosition.x}px`,
-        top:`${initialPosition.y}px`,
-        display: 'flex',
-        width: '200px',
-        height: '100px',
-        userSelect: 'none',
-        cursor:'move',
-        border: '1px solid #000',
-        backgroundColor: '#c36542',
-        overflowX: 'none',
-        overflowY: 'auto'
-    }}>
-        <p>{content}</p>
-    </div>
-  )
-}
+
+import { forwardRef } from "react";
+
+const Note = forwardRef((
+    { content, initialPosition, ...props },
+    ref
+) => {
+    return (
+        <div
+            ref={ref}
+            style={{
+                position: "absolute",
+                padding: "10px",
+                left: `${initialPosition?.x}px`,
+                top: `${initialPosition?.y}px`,
+                display: "flex",
+                width: "200px",
+                height: "100px",
+                userSelect: "none",
+                cursor: "move",
+                border: "1px solid #000",
+                backgroundColor: "#c36542",
+                // overflowX: "none",
+                // overflowY: "auto",
+            }}
+            {...props}
+        >
+            <p>📌 {content}</p>
+        </div>
+    );
+});
 
 export default Note;
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,7 +1,23 @@
+import { createRef, useEffect, useRef } from "react";
 import Note from "./Note";
 
 const Notes = ({ notes = {}, setNotes = () => {} }) => {
-    const notesStack = localStorage.getItem("notes") || notes;
+    let notesTobeRendered = [];
+    const noteRefs = useRef([]);
+
+    useEffect(() => {
+        const notesStack = localStorage.getItem("notes");
+        if (notesStack) {
+            notesTobeRendered = JSON.parse(notesStack);
+        } else {
+            notesTobeRendered = notes.map((note) => {
+                note.coords = determinePostion();
+                return { ...note };
+            });
+        }
+        setNotes(notesTobeRendered);
+        localStorage.setItem("notes", JSON.stringify(notesTobeRendered));
+    }, [notes.length]);
 
     const determinePostion = () => {
         const positionX = window.innerWidth - 250;
@@ -13,26 +29,75 @@ const Notes = ({ notes = {}, setNotes = () => {} }) => {
         };
     };
 
-    localStorage.setItem("notes", JSON.stringify());
+    const handleDragStart = (event, note) => {
+        const currentNote = noteRefs.current[note.id].current;
+        const rect = currentNote.getBoundingClientRect();
+        const startPos = note.coords;
 
-    return notesStack.map((note, i, stack) => {
-        if (!(note.x || note.y)) {
-            const coords = determinePostion();
-            stack[i].coords = coords;
-            return (
-                <Note
-                    key={note.id}
-                    content={note.text}
-                    initialPosition={coords}
-                />
-            );
-        }
+        const handleMouseMove = () => {
+            const offsetX = event.clientX - rect.left;
+            const offsetY = event.clientY - rect.top;
 
+            currentNote.style.left = `${offsetX}px`;
+            currentNote.style.top = `${offsetY}px`;
+        };
+
+        const handleMouseUp = () => {
+            document.removeEventListener("mousemove", handleMouseMove);
+            document.removeEventListener("mouseup", handleMouseUp);
+
+            const finalSnapshot = currentNote.getBoundingClientRect();
+            console.log(finalSnapshot);
+            const newPosition = { x: finalSnapshot.left, y: finalSnapshot.top };
+
+            if (checkForOverlap(note.id)) {
+                currentNote.style.left = `${startPos.x}px`;
+                currentNote.style.top = `${startPos.y}px`;
+            } else {
+                updateNewPosition(note.id, newPosition);
+            }
+        };
+        const checkForOverlap = (noteId) => {
+            const currentNote = noteRefs.current[noteId].current;
+            const currentRect = currentNote.getBoundingClientRect();
+            return notes.some((el) => {
+                if (el.id === noteId) return false;
+                const noteRect =
+                    noteRefs.current?.[el.id].current.getBoundingClientRect();
+                return !(
+                    currentRect.left > noteRect.right ||
+                    currentRect.right < noteRect.left ||
+                    currentRect.bottom < noteRect.top ||
+                    currentRect.top > noteRect.bottom
+                );
+            });
+        };
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
+    };
+
+    const updateNewPosition = (id, newPos) => {
+        const n = notes.findIndex((nt) => nt.id === id);
+        const stateToBeUpdated = [...notes];
+        stateToBeUpdated[n].coords = {...newPos};
+        console.log(stateToBeUpdated);
+        setNotes(stateToBeUpdated);
+        localStorage.setItem("notes", JSON.stringify(stateToBeUpdated));
+    };
+
+    return notes.map((note) => {
         return (
             <Note
                 key={note.id}
+                ref={
+                    noteRefs.current[note.id]
+                        ? noteRefs.current[note.id]
+                        : (noteRefs.current[note.id] = createRef())
+                }
                 content={note.text}
-                initialPosition={note.coords}
+                initialPosition={note?.coords}
+                onMouseDown={(e) => handleDragStart(e, note)}
+                className="sticky-note"
             />
         );
     });
